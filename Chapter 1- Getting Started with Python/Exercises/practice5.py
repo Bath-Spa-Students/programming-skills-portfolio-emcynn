cfc561cac1651ec4da72538ac2cf8b50cf8adfea
emcynn <mariezcauyan@gmail.com>
2023-10-05 11:42:59 +0400

    practice5.py

---
+++ Chapter 1- Getting Started with Python/Exercises/practice5.py
@@ -0,0 +1,7 @@
+#String Concatenation (four strings in different var and print the output as one string)
+name1 = "BathSpa"
+name2 = "University"
+name3 = "RAK"
+name4 = "Campus"
+
+print (name1, name2, name3, name4)
