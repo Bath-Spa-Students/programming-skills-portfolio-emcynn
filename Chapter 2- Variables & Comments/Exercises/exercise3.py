554d6aaae3dcb04906362f106c558f99001da33d
emcynn <mariezcauyan@gmail.com>
2023-10-10 14:13:10 +0400

    exercise3.py

---
+++ Chapter 2- Variables & Comments/Exercises/exercise3.py
@@ -0,0 +1,14 @@
+#Stripping Names
+name = "\tMariez Cauyan\n"
+
+print("Unmodified:")
+print(name)
+
+print("\nUsing lstrip():")
+print(name.lstrip())
+
+print("\nUsing rstrip():")
+print(name.rstrip())
+
+print("\nUsing strip():")
+print(name.strip())
