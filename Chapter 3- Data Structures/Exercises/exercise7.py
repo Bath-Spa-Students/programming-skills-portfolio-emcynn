31f39cda9d6bd4537190aa6d7c88852f3b3d7a60
emcynn <mariezcauyan@gmail.com>
2023-10-17 10:52:59 +0400

    exercise6.py

---
+++ Chapter 3- Data Structures/Exercises/exercise7.py
@@ -0,0 +1,33 @@
+#Seeing the World 
+locations = ['Japan', 'Italy', 'Europe', 'Philippines', 'New Zealand']
+
+print("Original Order:")
+print(locations)
+
+print("\nAlphabetical:")
+print(sorted(locations))
+
+print("\nOriginal Order:")
+print(locations)
+
+print("\nReverse Alphabetical:")
+print(sorted(locations, reverse=True))
+
+print("\nOriginal Order:")
+print(locations)
+
+print("\nReversed:")
+locations.reverse()
+print(locations)
+
+print("\nOriginal Order:")
+locations.reverse()
+print(locations)
+
+print("\nAlphabetical")
+locations.sort()
+print(locations)
+
+print("\nReverse Alphabetical")
+locations.sort(reverse=True)
+print(locations)
