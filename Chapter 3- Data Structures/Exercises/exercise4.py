ed864ea414178ea549cf0c6830a91313881ddd79
emcynn <mariezcauyan@gmail.com>
2023-10-17 10:52:33 +0400

    exercise4.py

---
+++ Chapter 3- Data Structures/Exercises/exercise4.py
@@ -0,0 +1,11 @@
+#Guest List 
+guests = ["Maryel Angela", "Seif Ibrahim", "Heba Albari"]
+
+name = guests[0].title()
+print("Greetings, " + name + "!" + " Please join us for a lovely dinner. We'd be delighted to have you for an enjoyable evening.")
+
+name = guests[1].title()
+print("Greetings, " + name + "!" + " Please join us for a lovely dinner. We'd be delighted to have you for an enjoyable evening.")
+
+name = guests[2].title()
+print("Greetings, " + name + "!" + " Please join us for a lovely dinner. We'd be delighted to have you for an enjoyable evening.")
