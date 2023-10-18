e8386a65b79d697cf6dc8c86736ee2c4ec0c10ef
emcynn <144207731+emcynn@users.noreply.github.com>
2023-10-18 11:21:07 +0400

    Update exercise1.py

--- Chapter 1- Getting Started with Python/Exercises/exercise1.py
+++ Chapter 1- Getting Started with Python/Exercises/exercise1.py
@@ -1,4 +1,13 @@
 #Print Strings 
+'''Write a Python program to print the following string in a specific format.
+
+Twinkle, twinkle, little star,
+	How I wonder what you are! 
+		Up above the world so high,   		
+		Like a diamond in the sky. 
+Twinkle, twinkle, little star, 
+	How I wonder what you are'''
+
 print (""" Twinkle Twinkle Little Star,
                 How I wonder what you are.
                Up above the world so high,
