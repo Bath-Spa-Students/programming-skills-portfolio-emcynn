4c88509bc0d0f15e663d681b25b7b62cf28e989c
emcynn <mariezcauyan@gmail.com>
2023-10-18 12:46:53 +0400

    Chapter 3 exercises

--- Chapter 3- Data Structures/Exercises/exercise6.py
+++ Chapter 3- Data Structures/Exercises/exercise6.py
@@ -1,13 +1,13 @@
 #Shrinking Guest List
-'''You just heard that one of your guests can’t make the
-dinner, so you need to send out a new set of invitations. You’ll have to think of
-someone else to invite.
+'''You just found out that your new dinner table won’t arrive in time for the dinner, and you have space for only two guests.
 
-•Start with your program from Exercise 3-4. Add a print() call at the end of your program stating the name of the guest who can’t make it.
+•Start with your program from Exercise 3-5. Add a new line that prints a message saying that you can invite only two people for dinner.
 
-•Modify your list, replacing the name of the guest who can’t make it with the name of the new person you are inviting.
+•Use pop() to remove guests from your list one at a time until only two names remain in your list. Each time you pop a name from your list, print a message to that person letting them know you’re sorry you can’t invite them to dinner.
 
-•Print a second set of invitation messages, one for each person who is still in your list.'''
+•Print a message to each of the two people still on your list, letting them know they’re still invited.
+
+•Use del to remove the last two names from your list, so you have an empty list. Print your list to make sure you actually have an empty list at the end of your program.'''
 
 guests = ["Maryel Angela", "Seif Ibrahim", "Heba Albari"]
 
