b8390418106f42ed5a7e7c1ed44bde9875e512bd
emcynn <mariezcauyan@gmail.com>
2023-10-25 09:56:57 +0400

    Chapter 7 exercises

---
+++ Chapter 7- Functions/Exercises/exercise1.py
@@ -0,0 +1,9 @@
+#Exercise 1: Message
+# Write a function called display_message() that prints one sentence telling everyone what you are learning about in this chapter. Call the function, and make sure the message displays correctly.
+
+def display_message():
+    msg = "In this chapter, I'm learning on how to use python functions."
+    print(msg)
+
+#Displaying the message by calling the function 
+display_message()
---
+++ Chapter 7- Functions/Exercises/exercise4.py
@@ -0,0 +1,16 @@
+#Exercise 4:  Large Shirts
+# Modify the make_shirt() function so that shirts are large by default with a message that reads I love Python. Make a large shirt and a medium shirt with the default message, and a shirt of any size with a different message.
+
+
+def make_shirt(size='large', message='I love Python!'):
+    print("\nI'm going to create a " + size + "-sized t-shirt.")
+    print('Its text design will be, "' + message + '"')
+
+#Call the function to make a large-sized shirt with the set default message
+make_shirt()
+
+#Call the function to make a medium-sized shirt with the set default message
+make_shirt(size='medium')
+
+#Call the function to make a medium-sized shirt with a different message
+make_shirt('medium', 'Just do it')
