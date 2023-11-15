b7f712ba84adbe5bfdc6ebe0fd8576056c950e7c
emcynn <144207731+emcynn@users.noreply.github.com>
2023-11-15 09:39:29 +0400

    Chapter 4 Exercises

--- Chapter 4- Control Flow/Exercises/exercise1.py
+++ Chapter 4- Control Flow/Exercises/exercise1.py
@@ -3,16 +3,12 @@
 •Write an if statement to test whether the alien’s color is green. If it is, print a message that the player just earned 5 points.
 •Write one version of this program that passes the if test and another that fails. (The version that fails will have no output.)'''
 
-#Pass: assigning color green to the Alien 
+#Pass ver.: assigning color green to the Alien 
 alien_color = 'green'
-
-#Program version that passes 
 if alien_color == 'green':
     print("You earned 5 points!")
 
-#Fail: assigning a different color  
+#Fail ver.: assigning a different color  
 alien_color = 'yellow'
-
-#Program version that fails 
 if alien_color == 'green':
     print("You earned 5 points!")
