17a1db05c762f5584f600610b5d16e55b2ef9876
emcynn <mariezcauyan@gmail.com>
2023-11-19 05:49:17 +0400

    Chapter 7 Exercises

--- Chapter 7- Functions/Exercises/exercise3.py
+++ Chapter 7- Functions/Exercises/exercise3.py
@@ -1,10 +1,10 @@
 #Exercise 3: T-Shirt  
-# Write a function called make_shirt() that accepts a size and the text of a message that should be printed on the shirt. The function should print a sentence summarizing the size of the shirt and the message printed on it. Call the function once using positional arguments to make a shirt. Call the function a second time using keyword arguments.
+#Write a function called make_shirt() that accepts a size and the text of a message that should be printed on the shirt. The function should print a sentence summarizing the size of the shirt and the message printed on it. Call the function once using positional arguments to make a shirt. Call the function a second time using keyword arguments.
 
 #Summary of the shirt design & sizes 
 def make_shirt(size, message):
-    print("\nI'm going to make a " + size + "-sized t-shirt.")
-    print('Its text design will be, "' + message + '"')
+    print(f"\nI'm going to make a {size}-sized t-shirt.")
+    print(f'Its text design will be, "{message}"')
 
 #Call the function using positional arguments
 make_shirt('small', 'Just do it!')
--- Chapter 7- Functions/Exercises/exercise4.py
+++ Chapter 7- Functions/Exercises/exercise4.py
@@ -1,10 +1,9 @@
-#Exercise 4:  Large Shirts
-# Modify the make_shirt() function so that shirts are large by default with a message that reads I love Python. Make a large shirt and a medium shirt with the default message, and a shirt of any size with a different message.
-
+#Exercise 4: Large Shirts
+#Modify the make_shirt() function so that shirts are large by default with a message that reads I love Python. Make a large shirt and a medium shirt with the default message, and a shirt of any size with a different message.
 
 def make_shirt(size='large', message='I love Python!'):
-    print("\nI'm going to create a " + size + "-sized t-shirt.")
-    print('Its text design will be, "' + message + '"')
+    print(f"\nI'm going to create a {size}-sized t-shirt.")
+    print(f'Its text design will be, "{message}".')
 
 #Call the function to make a large-sized shirt with the set default message
 make_shirt()
