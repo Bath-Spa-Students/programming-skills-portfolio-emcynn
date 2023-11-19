c5512f49f916e9470e3d5c59d4f7eb1b926f3dfc
emcynn <mariezcauyan@gmail.com>
2023-11-19 05:50:32 +0400

    Chapter 3 Exercises

--- Chapter 3- Data Structures/Exercises/exercise5.py
+++ Chapter 3- Data Structures/Exercises/exercise5.py
@@ -1,10 +1,8 @@
 #Exercise 5: Guest List 
-'''You just heard that one of your guests can’t make the dinner, so you need to send out a new set of invitations. You’ll have to think of someone else to invite.
-
-•Start with your program from Exercise 3-4. Add a print() call at the end of your program stating the name of the guest who can’t make it.
-•Modify your list, replacing the name of the guest who can’t make it with the name of the new person you are inviting.
-•Print a second set of invitation messages, one for each person who is still in your list.'''
-
+'''You just heard that one of your guests can't make the dinner, so you need to send out a new set of invitations. You'll have to think of someone else to invite.
+• Start with your program from Exercise 3-4. Add a print() call at the end of your program stating the name of the guest who can't make it.
+• Modify your list, replacing the name of the guest who can't make it with the name of the new person you are inviting.
+• Print a second set of invitation messages, one for each person who is still in your list.'''
 
 #List of invited guests
 guests = ["Maryel Angela", "Seif Ibrahim", "Heba Albari"]
@@ -16,7 +14,7 @@ print(f"Greetings, " + guests[1] + "!" + " Please join us for a lovely dinner. W
 
 print(f"Greetings, " + guests[2] + "!" + " Please join us for a lovely dinner. We'd be delighted to have you for an enjoyable evening.")
 
-#Seif can't attend dinner! Let's invite Yani instead.
+#Invite someone else since guest[1] can't attend 
 print(f"\nSorry, " + guests[1] + " can't make it to dinner.")
 del(guests[1])
 guests.insert(1, 'Yani Ariele')
--- Chapter 3- Data Structures/Exercises/exercise6.py
+++ Chapter 3- Data Structures/Exercises/exercise6.py
@@ -1,24 +1,31 @@
 #Exercise 6: Shrinking Guest List
-'''You just found out that your new dinner table won’t arrive in time for the dinner, and you have space for only two guests.
+'''You just found out that your new dinner table won't arrive in time for the dinner, and you have space for only two guests.
+• Start with your program from Exercise 3-5. Add a new line that prints a message saying that you can invite only two people for dinner.
+• Use pop() to remove guests from your list one at a time until only two names remain in your list. Each time you pop a name from your list, print a message to that person letting them know you're sorry you can't invite them to dinner.
+• Print a message to each of the two people still on your list, letting them know they're still invited.
+• Use del to remove the last two names from your list, so you have an empty list. Print your list to make sure you actually have an empty list at the end of your program.'''
 
-•Start with your program from Exercise 3-5. Add a new line that prints a message saying that you can invite only two people for dinner.
-•Use pop() to remove guests from your list one at a time until only two names remain in your list. Each time you pop a name from your list, print a message to that person letting them know you’re sorry you can’t invite them to dinner.
-•Print a message to each of the two people still on your list, letting them know they’re still invited.
-•Use del to remove the last two names from your list, so you have an empty list. Print your list to make sure you actually have an empty list at the end of your program.'''
-
+#Invite 3 guests to dinner
 guests = ["Maryel Angela", "Seif Ibrahim", "Heba Albari"]
-
 print(f"Greetings, " + guests[0] + "!" + " Please join us for a lovely dinner. We'd be delighted to have you for an enjoyable evening.")
-
 print(f"Greetings, " + guests[1] + "!" + " Please join us for a lovely dinner. We'd be delighted to have you for an enjoyable evening.")
-
 print(f"Greetings, " + guests[2] + "!" + " Please join us for a lovely dinner. We'd be delighted to have you for an enjoyable evening.")
+
+#Invite someone else since guest[1] can't attend 
+print(f"\nSorry, " + guests[1] + " can't make it to dinner.")
+del(guests[1])
+guests.insert(1, 'Yani Ariele')
+
+#Print the invitations again.
+print(f'\n{guests[0]}' + ", please come to dinner.")
+print(f'{guests[1]}' + ", please come to dinner.")
+print(f'{guests[2]}' + ", please come to dinner.")
 
 #The current table won't arrive on time
 print("\nUnfortunately, we can't accommodate the third guest.")
 
 name = guests.pop()
-print("Sorry, " + name.title() + " there's no room at the dinner table.\n")
+print("Sorry, " + name + "." + " There's no room at the dinner table.\n")
 
 #Invite the 2 remaining guests
 print(f"{guests[0]}" + ", you are still invited to dinner.")
@@ -27,6 +34,4 @@ print(f"{guests[1]}" + ", you are still invited to dinner.")
 #Clear out the list.
 del(guests[0])
 del(guests[0])
-
-#Prove the list is empty.
 print(guests)
